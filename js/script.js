c6ee81adef676732a389301fdf66108d0c28baa0
sonnyisnice <70600450+sonnyisnice@users.noreply.github.com>
2020-12-06 12:52:38 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+    
+        $(".vegetarianButton").click(function(){
+        $(".vegetarian").fadeToggle("slow");});
+    
+        $(".spicyButton").click(function(){
+        $(".Spicy").fadeToggle("slow");});
+    
+        $(".notSpicyButton").click(function(){
+        $(".notSpicy").fadeToggle("slow");});
+    
+        $(".seoulButton").click(function(){
+        $(".seoul").fadeToggle("slow");});
+    
+        $(".busanButton").click(function(){
+        $(".busan").fadeToggle("slow");});
+    
+        $(".daeguButton").click(function(){
+        $(".daegu").fadeToggle("slow");});
+    
+        $(".ulsanButton").click(function(){
+        $(".ulsan").fadeToggle("slow");});
+    
+        $(".incheonButton").click(function(){
+        $(".incheon").fadeToggle("slow");});
+      });
